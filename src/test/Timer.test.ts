7bf282bbf2cea965aa1204ca5d0220126806c791
Pablo Sabater <pabloosabaterr@gmail.com>
2020-12-10 23:43:14 +0100

    feat: init

---
+++ src/test/Timer.test.ts
@@ -0,0 +1,7 @@
+// // import Timer from "../src/Timer"
+// jest.mock("../src/Timer")
+
+// it('Podemos verificar que el consumidor llamó al constructor de la clase', () => {
+//   const timer = new Timer("30:20");
+//   expect(timer).toHaveBeenCalledTimes(1);
+// });
